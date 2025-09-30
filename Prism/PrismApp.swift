7e46e1c8ce268a78b17e00c808922741e7811733
okooo5km <okooo5km@gmail.com>
2025-09-30 20:10:46 +0800

    Initial Commit

---
+++ Prism/PrismApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrismApp.swift
+//  Prism
+//
+//  Created by 十里 on 2025/9/30.
+//
+
+import SwiftUI
+
+@main
+struct PrismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
